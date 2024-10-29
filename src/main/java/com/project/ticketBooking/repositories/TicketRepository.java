a50058f1b2b88aaccfe758a6d4144279ccb16a8e
kuangbuckk <kuangbuckk159@gmail.com>
2024-10-30 01:45:51 +0700

    Add action for TicketRepository

--- src/main/java/com/project/ticketBooking/repositories/TicketRepository.java
+++ src/main/java/com/project/ticketBooking/repositories/TicketRepository.java
@@ -3,5 +3,8 @@ package com.project.ticketBooking.repositories;
 import com.project.ticketBooking.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByTicketCategoryId(Long ticketCategoryId);
 }
