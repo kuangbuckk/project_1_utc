b38c4345109ecd437019ff03e61d2fac6a4e5986
kuangbuckk <kuangbuckk159@gmail.com>
2024-10-30 01:07:52 +0700

    Add User's related models

---
+++ src/main/java/com/project/ticketBooking/models/User.java
@@ -0,0 +1,4 @@
+package com.project.ticketBooking.models;
+
+public class User {
+}
---
+++ src/main/java/com/project/ticketBooking/models/UserAvatar.java
@@ -0,0 +1,28 @@
+package com.project.ticketBooking.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "user_avatars")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserAvatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Image URL cannot be null")
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @Min(value = 1, message = "User ID must be greater than 0")
+    private User user;
+}
