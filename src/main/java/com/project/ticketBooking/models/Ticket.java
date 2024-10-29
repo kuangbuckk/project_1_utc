712177281ec7fd8d610cba6781a2c47cb980d692
kuangbuckk <kuangbuckk159@gmail.com>
2024-10-30 01:07:10 +0700

    Update Ticket model

--- src/main/java/com/project/ticketBooking/models/Ticket.java
+++ src/main/java/com/project/ticketBooking/models/Ticket.java
@@ -1,9 +1,7 @@
 package com.project.ticketBooking.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 @Entity
@@ -13,22 +11,21 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Ticket extends BaseEntity{
+public class Ticket extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "name", nullable = false)
-    private Integer price;
-
-    @Column(name = "name")
-    private String description;
+    @Column(name = "status", nullable = false, columnDefinition = "varchar(20) default 'active'")
+    private String status;
 
     @ManyToOne
-    @JoinColumn
-    @Column(name = "event_id", nullable = false)
-    private Event eventId;
+    @JoinColumn(name = "ticket_category_id", nullable = false)
+    @Min(value = 1, message = "Ticket category ID must be greater than 0")
+    private TicketCategory ticketCategory;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @Min(value = 1, message = "User ID must be greater than 0")
+    private User user;
 }
