7b19c3a026cc62c7f49952b5a5a8c2a593cf5bfc
kuangbuckk <kuangbuckk159@gmail.com>
2024-10-30 16:05:34 +0700

    Add new actions for Repositories

--- src/main/java/com/project/ticketBooking/repositories/EventRepository.java
+++ src/main/java/com/project/ticketBooking/repositories/EventRepository.java
@@ -1,7 +1,11 @@
 package com.project.ticketBooking.repositories;
 
 import com.project.ticketBooking.models.Event;
+import com.project.ticketBooking.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByOrganizationId(Organization organizationId);
 }
--- src/main/java/com/project/ticketBooking/repositories/TicketCategoryRepository.java
+++ src/main/java/com/project/ticketBooking/repositories/TicketCategoryRepository.java
@@ -3,5 +3,9 @@ package com.project.ticketBooking.repositories;
 import com.project.ticketBooking.models.TicketCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketCategoryRepository extends JpaRepository<TicketCategory, Long> {
+    List<TicketCategory> findByEventId(Long eventId);
+    boolean existsByIdAndRemainingCountGreaterThan(Long categoryId, int count);
 }
