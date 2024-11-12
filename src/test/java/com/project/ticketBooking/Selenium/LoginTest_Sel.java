23e7834fb91ab885247897f63fa12dbe9c7b8730
boyslikeguy23 <tungxuanmai2003@gmail.com>
2024-11-12 16:45:13 +0700

    Reapply "Xong ham loginNoUsernameAndPassword"
    This reverts commit 334b3eb17ede1568c0fa6c6ae9885b7614852eaa.

--- src/test/java/com/project/ticketBooking/Selenium/LoginTest_Sel.java
+++ src/test/java/com/project/ticketBooking/Selenium/LoginTest_Sel.java
@@ -3,9 +3,7 @@ package com.project.ticketBooking.Selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,8 +11,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginTest_Sel {
     private WebDriver webDriver;
     private String homePageURL = "http://localhost:4200/";
@@ -31,9 +29,27 @@ public class LoginTest_Sel {
     public void loginNoUsernameAndPassword() throws Exception {
        WebElement loginButton = webDriver.findElement(By.id("login-button"));
        loginButton.click();
-       WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
-       wait.until(ExpectedConditions.urlToBe("http://localhost:4200/login"));
+       WebElement continueButton = webDriver.findElement(By.id("login-button-2"));
+       continueButton.click();
+       WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(2));
+       Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+       //String alertText = alert.getText();
+       assertNotNull(alert, "Alert không xuất hiện khi đăng nhập mà không nhập thông tin");
+       alert.accept();
+       //assertThrows(NoAlertPresentException.class, () -> webDriver.switchTo().alert());
+    }
 
+    @Test
+    public void loginWithUs() throws Exception {}
+    @After
+    public void tearDown() throws Exception {
+        try {
+            Thread.sleep(2000);
+            webDriver.close();
+            webDriver.quit();
+        } catch (Exception e) {
+            System.out.println("Đã xảy ra lỗi: " + e);
+        }
     }
 
 
