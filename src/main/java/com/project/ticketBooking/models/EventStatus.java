98c20c7d3727cac23346e681135104ad293d9444
kuangbuckk <kuangbuckk159@gmail.com>
2024-11-23 19:28:25 +0700

    add EventStatus enum

---
+++ src/main/java/com/project/ticketBooking/models/EventStatus.java
@@ -0,0 +1,8 @@
+package com.project.ticketBooking.models;
+
+public class EventStatus {
+    public static final String PENDING = "pending";
+    public static final String ACTIVE = "active";
+    public static final String CANCELLED = "cancelled";
+    public static final String COMPLETED = "completed";
+}
