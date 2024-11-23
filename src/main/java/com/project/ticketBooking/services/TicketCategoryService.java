e041f1344b566ce217c2a778d0d946bf6cae7db2
kuangbuckk <kuangbuckk159@gmail.com>
2024-11-23 19:27:58 +0700

    update TicketCategory actions for Organizers

--- src/main/java/com/project/ticketBooking/services/TicketCategoryService.java
+++ src/main/java/com/project/ticketBooking/services/TicketCategoryService.java
@@ -1,14 +1,19 @@
 package com.project.ticketBooking.services;
 
+import com.project.ticketBooking.component.JwtTokenUtils;
 import com.project.ticketBooking.dtos.TicketCategoryDTO;
 import com.project.ticketBooking.exceptions.DataNotFoundException;
+import com.project.ticketBooking.exceptions.PermissionDenyException;
 import com.project.ticketBooking.models.Event;
+import com.project.ticketBooking.models.Organization;
 import com.project.ticketBooking.models.TicketCategory;
 import com.project.ticketBooking.repositories.EventRepository;
+import com.project.ticketBooking.repositories.OrganizationRepository;
 import com.project.ticketBooking.repositories.TicketCategoryRepository;
 import com.project.ticketBooking.services.interfaces.ITicketCategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +22,8 @@ import java.util.List;
 public class TicketCategoryService implements ITicketCategoryService {
     private final TicketCategoryRepository ticketCategoryRepository;
     private final EventRepository eventRepository;
+    private final OrganizationRepository organizationRepository;
+    private final JwtTokenUtils jwtTokenUtils;
 
     @Override
     public List<TicketCategory> getAllTicketCategories() {
@@ -44,6 +51,28 @@ public class TicketCategoryService implements ITicketCategoryService {
     }
 
     @Override
+    @Transactional
+    public TicketCategory createTicketCategoryFromOrganization(
+            String token,
+            TicketCategoryDTO ticketDTO) throws DataNotFoundException
+    {
+        Event existingEvent = eventRepository.findById(ticketDTO.getEventId())
+                .orElseThrow(() -> new DataNotFoundException("Event not found"));
+        Long organizationId = Long.valueOf(jwtTokenUtils.extractOrganizationId(token));
+        Organization existingOrganization = organizationRepository
+                .findById(organizationId)
+                .orElseThrow(() -> new DataNotFoundException("Organization not found"));
+
+        TicketCategory ticketCategory = TicketCategory.builder()
+                .categoryName(ticketDTO.getCategoryName())
+                .price(ticketDTO.getPrice())
+                .remainingCount(ticketDTO.getRemainingCount())
+                .event(existingEvent)
+                .build();
+        return ticketCategoryRepository.save(ticketCategory);
+    }
+
+    @Override
     public TicketCategory updateTicketCategory(Long id, TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException {
         TicketCategory ticketCategory = ticketCategoryRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("Ticket Category not found"));
@@ -55,6 +84,25 @@ public class TicketCategoryService implements ITicketCategoryService {
         ticketCategory.setPrice(ticketCategoryDTO.getPrice());
         ticketCategory.setRemainingCount(ticketCategoryDTO.getRemainingCount());
 //        ticketCategory.setEvent(existingEvent);
+        return ticketCategoryRepository.save(ticketCategory);
+    }
+
+    @Override
+    public TicketCategory updateTicketCategoryFromOrganization(String token, Long id, TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException, PermissionDenyException {
+        TicketCategory ticketCategory = ticketCategoryRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Ticket Category not found"));
+
+        Event existingEvent = eventRepository.findById(ticketCategoryDTO.getEventId())
+                .orElseThrow(() -> new DataNotFoundException("Event not found"));
+
+        Long organizationId = Long.valueOf(jwtTokenUtils.extractOrganizationId(token));
+        if (!existingEvent.getOrganization().getId().equals(organizationId)) {
+            throw new PermissionDenyException("You can not update other organization's ticket category");
+        }
+
+        ticketCategory.setCategoryName(ticketCategoryDTO.getCategoryName());
+        ticketCategory.setPrice(ticketCategoryDTO.getPrice());
+        ticketCategory.setRemainingCount(ticketCategoryDTO.getRemainingCount());
         return ticketCategoryRepository.save(ticketCategory);
     }
 
--- src/main/java/com/project/ticketBooking/services/interfaces/ITicketCategoryService.java
+++ src/main/java/com/project/ticketBooking/services/interfaces/ITicketCategoryService.java
@@ -2,6 +2,7 @@ package com.project.ticketBooking.services.interfaces;
 
 import com.project.ticketBooking.dtos.TicketCategoryDTO;
 import com.project.ticketBooking.exceptions.DataNotFoundException;
+import com.project.ticketBooking.exceptions.PermissionDenyException;
 import com.project.ticketBooking.models.TicketCategory;
 
 import java.util.List;
@@ -10,7 +11,11 @@ public interface ITicketCategoryService {
     List<TicketCategory> getAllTicketCategories();
     TicketCategory getTicketCategoryById(Long id) throws DataNotFoundException;
     TicketCategory createTicketCategory(TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException;
+    TicketCategory createTicketCategoryFromOrganization(
+            String token, TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException;
     TicketCategory updateTicketCategory(Long id, TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException;
+    TicketCategory updateTicketCategoryFromOrganization(
+            String token, Long id, TicketCategoryDTO ticketCategoryDTO) throws DataNotFoundException, PermissionDenyException;
     void deleteTicketCategory(Long id);
     List<TicketCategory> getTicketCategoriesByEventId(Long eventId) throws DataNotFoundException;
     List<TicketCategory> getTicketCategoriesByIds(List<Long> ticketCategoryIds);
