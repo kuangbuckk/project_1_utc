df7cd6a4b1fd6e725a47c3ec1ca9797837628d26
kuangbuckk <kuangbuckk159@gmail.com>
2024-12-02 02:02:42 +0700

    fix(event): query active status events only

--- src/main/java/com/project/ticketBooking/repositories/EventRepository.java
+++ src/main/java/com/project/ticketBooking/repositories/EventRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("SELECT e FROM Event e WHERE e.status = 'active'")
     Page<Event> findAll(Pageable pageable);
     List<Event> findAllByOrganizationId(Long organizationId);
     Page<Event>  findByOrganizationId(Long organizationId, Pageable pageable);
